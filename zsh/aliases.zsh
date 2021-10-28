f653bf2eb1161e81371672db571fc82a7c090bc7
James Wilson <jw@jameswilson.io>
2021-10-28 20:52:43 +1100

    added aliases from old mac

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,10 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias ls="exa --git --color=automatic -a -l -b"
+
+alias diff="/usr/local/bin/diff"
+
+export PATH="$PATH:/Applications/ARM/bin"
+alias make="make -j11"
