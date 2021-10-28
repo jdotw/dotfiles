59946a3694892e8d12746dada2df657bf637db1c
jdotw <jw@jameswilson.io>
2021-10-28 21:49:05 +1100

    nvm fixes

--- node/install.sh
+++ node/install.sh
@@ -6,4 +6,8 @@ then
   fi
 fi
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
 nvm install --lts
