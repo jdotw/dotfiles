a69ddffde5b3f948831f4e34fcfb050c1ade0c24
James Wilson <jw@jameswilson.io>
2021-10-29 14:13:33 +1100

    dynamic gcc parallelization

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,5 +6,6 @@ alias ls="exa --git --color=automatic -a -l -b"
 
 alias diff="/usr/local/bin/diff"
 
+GCC_PARALLEL=$(($(sysctl -n hw.ncpu) - 1))
 export PATH="$PATH:/Applications/ARM/bin"
-alias make="make -j11"
+alias make="make -j$GCC_PARALLEL"
