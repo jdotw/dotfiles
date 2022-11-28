c29187d4184c68070a2ca80316dc32b55af12ece
James Wilson <jw@jameswilson.io>
2022-11-28 21:02:29 +1100

    introduced minimal setup

--- iterm/setup.sh
+++ iterm/setup.sh
@@ -1,1 +1,6 @@
+if [[ $DOTFILES_MINIMAL == 1 ]]; then
+  echo "Skipping iterm/setup.sh due to minimal setup..."
+  exit 0
+fi
+
 cp ~/.dotfiles/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
