f5aeaec63aa2bdcf4b380eae35fabcdc121c19c6
James Wilson <jw@jameswilson.io>
2022-11-28 22:04:29 +1100

    only use kubectl if it exists

--- kubernetes/completion.zsh
+++ kubernetes/completion.zsh
@@ -1,2 +1,4 @@
-source <(kubectl completion zsh)
-source <(kubectl completion zsh | sed 's/kubectl/kk/g')
+if [ -e $PATH/kubectl ]; then
+  source <(kubectl completion zsh)
+  source <(kubectl completion zsh | sed 's/kubectl/kk/g')
+fi
