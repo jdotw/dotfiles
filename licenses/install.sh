044fe118df7c2b93454f0ac2cd5acc0cf364940d
James Wilson <jw@jameswilson.io>
2022-11-28 20:56:54 +1100

    implemented skipping install scripts for minimal install

--- licenses/install.sh
+++ licenses/install.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 
+if [[ $DOTFILES_MINIMAL == 1 ]]; then
+  echo "Skipping licenses/install.sh due to minimal setup..."
+  exit 0
+fi
+
 DAISYDISK_PATH=~/Library/Application\ Support/DaisyDisk
 if ! [ -d $DAISYDISK_PATH ]; then
   mkdir $DAISYDISK_PATH
